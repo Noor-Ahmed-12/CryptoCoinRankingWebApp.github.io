ef29f5124e7183d9ea3ef3cb516771124213cee2
Noor Ahmed Shaikh <60597502+Noor-Ahmed-12@users.noreply.github.com>
2022-06-19 10:10:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let baseURL = "https://api.coinranking.com/v2/coins";
+let proxyURL = "https://cors-anywhere.herokuapp.com/";
+let apiKey = "coinranking31fc5c78c6578c364531d7fd5c2a06c7e84caa6296d91181";
+
+fetch(`${proxyURL}${baseURL}`,{
+    method: "GET",
+    headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': `${apiKey}`,
+        'Access-Control-Allow-Origin': '*'
+    }
+}).then(response => {
+    if (response.ok) {
+        response.json().then((json) => {
+            // coins data from API
+            let coinsData = json.data.coins;
+            if (coinsData.length > 0) {
+                var cryptoCoins =""
+            }
+            coinsData.forEach((coins) => {
+                cryptoCoins +="<tr>"
+                cryptoCoins +=`<td> ${coins.uuid} </td>`;
+                cryptoCoins +=`<td> ${coins.rank} </td>`;
+                cryptoCoins +=`<td> <img src="${coins.iconUrl}" width="50px" height="50"> </td>`;
+                cryptoCoins +=`<td> ${coins.name} </td>`;
+                cryptoCoins +=`<td> ${coins.symbol} </td>`;
+                cryptoCoins +=`<td>$${Math.round(coins.price)} Billion</td>`;
+                cryptoCoins +=`<td> ${coins.tier} </td>`;
+                cryptoCoins +=`<td> ${coins.btcPrice} </td>`;
+                "</tr>";
+            });
+            document.getElementById("data").innerHTML = cryptoCoins;
+        })
+    }
+}).catch((error) => {
+    console.log(error);
+})
